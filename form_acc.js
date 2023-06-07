e4d70850576003995c86b6fd094d896e655246f0
anthonyalando8 <103507949+anthonyalando8@users.noreply.github.com>
2023-06-07 16:14:02 +0300

    Add files via upload

---
+++ form_acc.js
@@ -0,0 +1,15 @@
+var formAcc = document.getElementById("formAcc");
+if(window.innerWidth <= 450){
+  formAcc.style.width = "95%";
+}
+else{
+  formAcc.style.width = "450px";
+}
+
+if(window.innerHeight < 520){
+  formAcc.classList.remove("center-container");
+  formAcc.style.margin ="auto";
+}
+else{
+  formAcc.classList.add("center-container");
+}
--- index.js
+++ index.js
@@ -23,21 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-var formAcc = document.getElementById("formAcc");
-if(window.innerWidth <= 450){
-  formAcc.style.width = "95%";
-}
-else{
-  formAcc.style.width = "450px";
-}
-if(window.innerHeight < 520){
-  formAcc.classList.remove("center-container");
-  formAcc.style.margin ="auto";
-}
-else{
-  formAcc.classList.add("center-container");
-} 
+ 
 
 
 //stop scrolling
