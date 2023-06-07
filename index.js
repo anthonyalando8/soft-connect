9671c40d806e44b85712f69cafc5f07b7418d4e0
anthonyalando8 <103507949+anthonyalando8@users.noreply.github.com>
2023-06-07 22:35:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
