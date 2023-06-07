c6df84d8b993d8937f3f37afa13a62659d557d5e
anthonyalando8 <103507949+anthonyalando8@users.noreply.github.com>
2023-06-07 16:24:37 +0300

    Add files via upload

--- form_acc.js
+++ form_acc.js
@@ -13,3 +13,16 @@ if(window.innerHeight < 520){
 else{
   formAcc.classList.add("center-container");
 }
+
+document.onreadystatechange = function(){
+if(document.readyState !== "complete"){
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#spinner").style.visibility = "visible";
+    //disableScrolling();
+}
+else{
+    document.querySelector("#spinner").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+    //enableScrolling();
+}
+}
